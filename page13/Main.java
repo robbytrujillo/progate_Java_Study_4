37c69524b2d0930ea06cc48497024c8a896be70f
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-09-28 07:02:19 +0700

    Progate Java Study 4

---
+++ page13/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+  
+    Person person1 = new Person("Murni", "Aminah", 27, 1.6, 50.0);
+    person1.printData();
+    
+    Person person2 = new Person("Yahya", "Rizal", 65, 1.75, 80.0);
+    person2.printData();
+    
+    // Print "Total: ____ orang."
+    System.out.println("Total: " + Person.count +  " orang.");
+    
+  }
+}
---
+++ page5/Person.java
@@ -0,0 +1,9 @@
+class Person {
+  // Deklarasikan field instance `name`
+public String name;
+  
+
+  public void hello() {
+    System.out.println("Hello");
+  }
+}
